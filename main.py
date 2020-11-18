ffd3e1524a1e815f642cbb31ee23094aa233e32c
MaxVukovich <maxvukov@gmail.com>
2020-11-18 09:44:59 -0800

    final change + raspberry pi

--- main.py
+++ main.py
@@ -81,4 +81,4 @@ def showform():
 if __name__ == "__main__":
     # runs the application on the repl development server
     app.secret_key = os.urandom(12)
-    app.run(debug=True)
+    app.run(debug=True, port='8080', host='192.168.1.103')
